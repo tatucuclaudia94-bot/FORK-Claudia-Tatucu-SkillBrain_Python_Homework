dee861ca95bf62b95cd309261e5caec0de4eccf7
Claudia Tatucu <tatucu.claudia94@gmail.com>
2025-08-10 16:45:40 +0300

    Add files via upload

---
+++ Rezolvare tema 1.py
@@ -0,0 +1,19 @@
+# Rezolvare Tema 1
+
+oras_preferat = "Deva"  # str
+print(type(oras_preferat))
+
+inaltimea_ta = 1.70  # float
+print(type(inaltimea_ta))
+
+mesaj_azi = "Am fost in oras"  # str
+print(type(mesaj_azi))
+
+plimbare_azi = "Nu m-am plimbat azi"  # str
+print(type(plimbare_azi))
+
+print("\n\t Survey in care participantul a raspuns:")
+print("Orasul preferat:", oras_preferat)
+print("Inaltimea:", inaltimea_ta)
+print("Mesajul zilei:", mesaj_azi)
+print("Plimbare azi:", plimbare_azi)
